61156353943fb6dd51c1a324e418b747e6bada6e
kuwood <p4nic510@gmail.com>
2016-08-10 13:20:17 -0600

    Add drawer assignment, word assignment and disconnection handler

--- public/main.js
+++ public/main.js
@@ -1,59 +1,125 @@
 const socket = io()
 
 let pictionary = () => {
-    let canvas, context, guessBox
+    let canvas, context, guessBox, answer, drawer, id
+    let drawing = false
 
-    let onKeyDown = (event) => {
+    let hideCanvas = () => {
+        $('#wait').show()
+        $('#canvas').hide()
+    }
+
+    let showCanvas = () => {
+        $('#wait').hide()
+        $('#canvas').show()
+    }
+
+    let winAlert = statement => {
+        alert(statement)
+        //select, create context, set dimensions and clear canvas
+        canvas = $('canvas')
+        context = canvas[0].getContext('2d')
+        canvas[0].width = canvas[0].offsetWidth;
+        canvas[0].height = canvas[0].offsetHeight;
+        context.clearRect(0,0, canvas.height, canvas.width)
+    }
+
+    let setAnswer = ans => {
+        answer = ans
+        $('#guess').hide()
+        $('#instructions').show()
+        $('#instructions').text('draw the word "' + answer + '"')
+        $('#last-guess').show()
+        $('#last-guess').text('')
+    }
+
+    //assigns the drawer and the guessers
+    let drawerCheck = clientId => {
+        if (clientId === id) {
+            drawer = true
+            $('h1').text('Pictionary (drawer)')
+        } else {
+            drawer = false
+            $('h1').text('Pictionary (guesser)')
+            $('#guess').show()
+            $('#last-guess').hide()
+            $('#instructions').hide()
+        }
+    }
+
+    //when enter is pressed it emits input value and clears the input
+    let onKeyDown = event => {
         if (event.keyCode != 13) {
-            return;
+            return
         }
 
-        console.log(guessBox.val())
         let guess = guessBox.val()
-        socket.emit('guess', guess)
+        socket.emit('guess', id, guess)
         guessBox.val('')
     }
 
+    //guessbox listener
     guessBox = $('#guess input')
     guessBox.on('keydown', onKeyDown)
 
-    let lastGuess = (guess) => {
-        $('#last-guess').text(guess)
+    let lastGuess = guess => {
+        $('#last-guess').text('most recent guess: ' + guess)
     }
 
-    let draw = (position) => {
-        context.beginPath();
+    //draw on canvas
+    let draw = position => {
+        context.beginPath()
         context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
-        context.fill();
-    };
+                         6, 0, 2 * Math.PI)
+        context.fill()
+    }
 
-    let drawing = false;
-
-    canvas = $('canvas');
-    context = canvas[0].getContext('2d');
-    canvas[0].width = canvas[0].offsetWidth;
-    canvas[0].height = canvas[0].offsetHeight;
-    canvas.on('mousedown', (event) => {
-        drawing = true
+    //select, create context, set dimensions of canvas
+    canvas = $('canvas')
+    context = canvas[0].getContext('2d')
+    canvas[0].width = canvas[0].offsetWidth
+    canvas[0].height = canvas[0].offsetHeight
+    //checks if your the drawer on mouse down
+    canvas.on('mousedown', event => {
+        if (drawer === true) {
+            drawing = true
+        } else {
+            alert('sorry the "guesser" cannot draw.')
+        }
     })
-    canvas.on('mouseup', (event) => {
-        console.log('STOP DRAWINGGG');
+
+    //stops drawing on mouse up
+    canvas.on('mouseup', event => {
         drawing = false
     })
-    canvas.on('mousemove', (event) => {
-        let offset = canvas.offset();
+
+    //canvas mouse move listener
+    canvas.on('mousemove', event => {
+        //assign the canvas offset
+        let offset = canvas.offset()
+        //subtract offset of canvas relative to top left of canvas
         let position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
+                        y: event.pageY - offset.top}
+
+        //check if drawing has been enabled
         if (drawing === true) {
-            draw(position);
+            //draw position to canvas
+            draw(position)
+            //send position to server to share
             socket.emit('draw', position)
         }
-    });
+    })
+    //listeners
+    socket.on('setId', clientId => {id = clientId})
+    socket.on('setDrawer', drawerCheck)
+    socket.on('answer', setAnswer)
     socket.on('draw', draw)
+    socket.on('correct', winAlert)
     socket.on('guess', lastGuess)
-};
+    socket.on('hideCanvas', hideCanvas)
+    socket.on('showCanvas', showCanvas)
+}
 
 $(document).ready(() => {
-    pictionary();
-});
+    pictionary()
+})
--- server.js
+++ server.js
@@ -8,17 +8,78 @@ app.use(express.static('public'))
 const server = http.Server(app)
 const io = socket_io(server)
 
-io.on('connection', (socket) => {
+let numConnections = 0
+const words = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
 
-    socket.on('draw', (position) => {
-        console.log('drawing:', position);
+let answer
+let usersArray = []
+let drawer
+
+io.on('connection', socket => {
+    numConnections++
+    usersArray.push(socket.client.id)
+    if (numConnections === 1) {
+        answer = words[Math.floor(Math.random()*words.length)]
+        drawer = socket.client.id
+        socket.emit('setId', socket.client.id)
+        socket.emit('setDrawer', socket.client.id)
+        socket.emit('answer', answer)
+        io.emit('hideCanvas')
+    } else {
+        socket.emit('setId', socket.client.id)
+        io.emit('showCanvas')
+    }
+
+    socket.on('draw', position => {
         socket.broadcast.emit('draw', position)
     })
 
-    socket.on('guess', (guess) => {
-        socket.broadcast.emit('guess', guess)
+    socket.on('guess', (id, guess) => {
+        if (guess === answer) {
+            let correct = answer + " is correct"
+            io.sockets.emit('correct', correct)
+            answer = words[Math.floor(Math.random()*words.length)]
+            drawer = id
+            io.emit('setDrawer', id)
+            socket.emit('answer', answer)
+        } else {
+            socket.broadcast.emit('guess', guess)
+        }
     })
 
+    socket.on('disconnect', () => {
+        let id = usersArray.indexOf(socket.client.id)
+        usersArray.splice(id, 1)
+        numConnections--
+        if (numConnections === 1) {
+            io.emit('hideCanvas')
+            answer = words[Math.floor(Math.random()*words.length)]
+            drawer = usersArray[0]
+            io.emit('setDrawer', usersArray[0])
+            io.emit('answer', answer)
+        } else if (socket.client.id === drawer) {
+            socket.broadcast.to(usersArray[0]).emit('welcome', 'welcome')
+            drawer = usersArray[Math.floor(Math.random()*usersArray.length)]
+            io.emit('setDrawer', drawer)
+            answer = words[Math.floor(Math.random()*words.length)]
+            io.to('/#'+drawer).emit('answer', answer)
+        }
+        console.log(usersArray)
+    })
 })
 
 server.listen(8080)
